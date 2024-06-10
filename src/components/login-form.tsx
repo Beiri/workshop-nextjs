7c6e5710b6f270ff6c2f1ce437ab644e5fbb2c8e
Beiri Alife <beiri.al@outlook.com>
2024-06-10 20:38:00 -0300

    Login Form Customization

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -2,30 +2,48 @@
 
 import { authenticate } from '@/lib/actions';
 import { useFormState, useFormStatus } from 'react-dom';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 export function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
-    <form action={dispatch}>
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        className="border border-red-700"
-      />
-
-      <label htmlFor="password">Password</label>
-      <input
-        id="password"
-        type="password"
-        name="password"
-        className="border border-red-700"
-      />
-
-      <LoginButton />
-      {errorMessage}
+    <form className="w-80" action={dispatch}>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="email@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" name="password" required />
+          </div>
+        </CardContent>
+        <CardFooter className="flex-col gap-4">
+          <LoginButton />
+          <div className="text-destructive text-sm">{errorMessage}</div>
+        </CardFooter>
+      </Card>
     </form>
   );
 }
@@ -34,8 +52,8 @@ function LoginButton() {
   const { pending } = useFormStatus();
 
   return (
-    <button type="submit" disabled={pending}>
-      Login
-    </button>
+    <Button className="w-full" type="submit" disabled={pending}>
+      Log in
+    </Button>
   );
 }
