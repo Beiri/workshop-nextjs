fda952b2c53320c1beebe92617a0c93394dba21e
Beiri Alife <beiri.al@outlook.com>
2024-06-10 22:12:23 -0300

    Admin Navigation

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import { AdminNavigation } from '@/components/admin-navigation';
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <AdminNavigation />
+      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+        {children}
+      </main>
+    </div>
+  );
+}
---
+++ src/components/admin-navigation-mobile.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Button } from './ui/button';
+import { Menu, Package2 } from 'lucide-react';
+import Link from 'next/link';
+import { AdminNavigationLink } from './admin-navigation-link';
+import { useState } from 'react';
+
+export function AdminNavigationMobile({ links }: { links: any }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+          <Menu className="h-5 w-5" />
+          <span className="sr-only">Toggle navigation menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <nav className="grid gap-6 text-lg font-medium">
+          <Link
+            href="/dashboard"
+            className="flex items-center gap-2 text-lg font-semibold"
+          >
+            <Package2 className="h-6 w-6" />
+            <span className="sr-only">Acme Inc</span>
+          </Link>
+          {links.map((link) => (
+            <AdminNavigationLink
+              href={link.href}
+              onClick={() => setOpen(false)}
+            >
+              {link.name}
+            </AdminNavigationLink>
+          ))}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+}
