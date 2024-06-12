494b3ceca0d7e534db281dfe38e7f239a4615230
Beiri Alife <beiri.al@outlook.com>
2024-06-11 22:56:17 -0300

    Jobs creation page and action

---
+++ src/app/dashboard/jobs/create/page.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { FormError } from '@/components/form-error';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Switch } from '@/components/ui/switch';
+import { Textarea } from '@/components/ui/textarea';
+import { jobCreate } from '@/lib/actions';
+import Link from 'next/link';
+import { useFormState } from 'react-dom';
+
+export default function JobsCreate() {
+  const [error, action] = useFormState(jobCreate, undefined);
+
+  return (
+    <>
+      <h1>Create Job</h1>
+      <form action={action} className="space-y-4 max-w-screen-lg">
+        <div className="space-y-2">
+          <Label htmlFor="title">Title</Label>
+          <Input id="title" type="text" name="title" />
+          {error?.title && <FormError>{error.title}</FormError>}
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="content">Content</Label>
+          <Textarea id="content" name="content" />
+          {error?.content && <FormError>{error.content}</FormError>}
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Switch id="published" name="published" />
+          <Label htmlFor="published">Published</Label>
+        </div>
+
+        <div className="space-x-4">
+          <Button type="submit">Save</Button>
+          <Button variant="destructive" asChild>
+            <Link href="/dashboard/jobs" role="button">
+              Cancel
+            </Link>
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+}
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,6 +1,9 @@
 'use server';
-import { signIn } from '@/auth';
+import { auth, signIn } from '@/auth';
+import prisma from '@/db/db';
 import { AuthError } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
 
 export async function authenticate(
   prevState: string | undefined,
@@ -18,5 +21,42 @@ export async function authenticate(
       }
     }
     throw error;
+  }
+}
+
+export async function jobCreate(prevState: unknown, formData: FormData) {
+  const parsedData = z
+    .object({
+      title: z.string().min(1, { message: 'Title field is required.' }),
+      content: z.string().min(1, { message: 'Content field is required.' }),
+      published: z.coerce.boolean(),
+    })
+    .safeParse(Object.fromEntries(formData.entries()));
+
+  if (!parsedData.success) {
+    return parsedData.error.formErrors.fieldErrors;
+  }
+
+  const session = await auth();
+
+  if (session?.user?.email) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (user) {
+      await prisma.jobs.create({
+        data: {
+          ...parsedData.data,
+          authorId: user.id,
+        },
+      });
+
+      redirect('/dashboard/jobs');
+    }
+  } else {
+    redirect('/login');
   }
 }
